3fa36d64998753a69db34ef1013d132a9a95b99b
weiy <503945930@qq.com>
2016-06-29 19:34:04 +0800

    project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var router = require('koa-router')();
 
+//
+
 router.get('/', function *(next) {
+  var start = new Date;
+  var ms = new Date - start;
+
   yield this.render('index', {
     title: 'Hello World Koadda!'
   });
 });
+
+//logger
+
+
 
 module.exports = router;
